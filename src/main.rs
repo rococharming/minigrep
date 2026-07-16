d737cd624e62faf6b760bd485a8c668b60dadd51
SongPengfei <1229393891@qq.com>
2026-07-17 00:58:58 +0800

    ci: add Rust checks

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,16 @@ use std::env;
 use std::fs;
 
 fn main() {
-
     // 获取所有命令行参数
     // std::env::args() 返回的是 Args，它是一个迭代器类型
     let args = env::args().collect::<Vec<String>>();
     // 注意，命令行的第一个参数（索引为0）是程序名
-    let query = &args[1];        // 获取查询词
-    let file_path = &args[2];    // 获取文件路径
+    let query = &args[1]; // 获取查询词
+    let file_path = &args[2]; // 获取文件路径
     println!("Searching for {}", query);
     println!("In file {}", file_path);
 
     // 读取文件内容并打印
-    let contents = fs::read_to_string(file_path)
-        .expect("Something went wrong reading the file");
+    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
     println!("\nWith text:\n{contents}");
-
 }
