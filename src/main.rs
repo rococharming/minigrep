43f293d2d55b4c26766f05dfb0c1dee0559fd7f7
rococharming <120177398+rococharming@users.noreply.github.com>
2026-07-17 03:15:03 +0800

    feat: search query in file (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
+use minigrep::Config;
 use std::env;
-use std::fs;
+use std::process;
 
 fn main() {
     // 获取所有命令行参数
     // std::env::args() 返回的是 Args，它是一个迭代器类型
     let args = env::args().collect::<Vec<String>>();
-    // 注意，命令行的第一个参数（索引为0）是程序名
-    let query = &args[1]; // 获取查询词
-    let file_path = &args[2]; // 获取文件路径
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
 
-    // 读取文件内容并打印
-    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
-    println!("\nWith text:\n{contents}");
+    // 获取配置
+    let config = Config::new(args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err); // 参数不够退出程序
+        process::exit(1);
+    });
+
+    // 根据配置运行代码
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
 }
